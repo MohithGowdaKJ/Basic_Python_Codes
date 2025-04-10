10144ce3074000c691681a761e9088d57be7cbcd
MohithGowdaKJ <mohith1234gowda@gmail.com>
2025-04-10 14:55:07 +0530

    initial file

---
+++ Basics.py
@@ -0,0 +1,13 @@
+#Scripting way
+print('Hello World')
+
+#Procedural way 
+def Method():
+    print("Hello world")
+
+#Object Oriented way
+class Demo:
+    def Method():
+        print("Hello World")
+D1 = Demo()    
+D1.Method()
