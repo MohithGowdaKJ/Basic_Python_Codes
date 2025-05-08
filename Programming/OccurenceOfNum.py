b5153e54d2768b1e59b9d771fa39202ada308977
MohithGowdaKJ <mohith1234gowda@gmail.com>
2025-05-08 10:24:37 +0530

    Added Files

--- Programming/OccurenceOfNum.py
+++ Programming/OccurenceOfNum.py
@@ -5,13 +5,17 @@ for i in li:
         di[i] = di[i] + 1
     else:
         di[i] = 1
+print(di)
 for digit,count in di.items():
     print(f"{digit} Occures {count} time(s)")
 
 '''
-1 2 2 3 3 3 4 4 4 4
-1 Occures 1 time(s)
-2 Occures 2 time(s)
-3 Occures 3 time(s)
-4 Occures 4 time(s)
+1 2 3 2 4 6 1 3 5 2 1 
+{1: 3, 2: 3, 3: 2, 4: 1, 6: 1, 5: 1}
+1 Occures 3 time(s)
+2 Occures 3 time(s)
+3 Occures 2 time(s)
+4 Occures 1 time(s)
+6 Occures 1 time(s)
+5 Occures 1 time(s)
 '''
